dc875ba586031ac6e800f041d45d07c1ee3db835
tangchao <1617564083@qq.com>
2026-02-21 16:39:15 +0800

    补全BUG

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,21 +3,25 @@ import Antd from "ant-design-vue";
 import App from "./App.vue";
 import * as VueRouter from "vue-router";
 import routes from "./configs/routes";
-import { createPinia } from "pinia";
+import { createPinia, setActivePinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import "ant-design-vue/dist/antd.css";
 import "./style.css";
 import { useUserStore } from "./core/userStore";
+
+const app = createApp(App);
+
+// 状态管理
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+setActivePinia(pinia); // 显式激活 Pinia
 
 // 路由
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes,
 });
-
-// 状态管理
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
 
 // 路由守卫
 router.beforeEach((to, from, next) => {
@@ -29,4 +33,4 @@ router.beforeEach((to, from, next) => {
   }
 });
 
-createApp(App).use(Antd).use(router).use(pinia).mount("#app");
+app.use(Antd).use(router).mount("#app");
